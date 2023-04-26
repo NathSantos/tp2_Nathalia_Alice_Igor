4ba752e00e14f1a9a448badd1f1819a4fd94780e
Nathália Santos <63311872+NathSantos@users.noreply.github.com>
2023-04-26 13:45:01 -0400

    Add files via upload

--- structs.h
+++ structs.h
@@ -1,3 +1,6 @@
+#define BUCKET_SIZE 8192
+#define NUMBER_OF_BUCKETS 5
+
 struct registro_t {
     int id;
     int tam_titulo;     // tamanho do campo título
@@ -6,7 +9,6 @@ struct registro_t {
     int tam_autores;    // tamanho do campo autores
     char autores[151];
     int citacoes;
-    int tam_atualizacao;    // tamanho do campo atualização
     char atualizacao[20];
     int tam_snippet;        // tamanho do campo snippet
     char snippet[1025];
@@ -20,4 +22,9 @@ struct bucket_t {
     registro_t *registro3;  // ponteiro para o registro 3
     registro_t *registro4;  // ponteiro para o registro 4
     bucket_t *proxOverflow;      // ponteiro para o próximo bucket de overflow, caso exista
+};
+
+// Define uma estrutura de dados para o arquivo de dados binários
+struct arquivo_t {
+    bucket_t buckets[NUMBER_OF_BUCKETS];
 };
