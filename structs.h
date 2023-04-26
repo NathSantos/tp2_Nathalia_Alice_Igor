d7d992dfedb650fb5b7e42962b89623a6db3d51b
Nathália Santos <63311872+NathSantos@users.noreply.github.com>
2023-04-26 15:09:42 -0400

    Add files via upload

--- structs.h
+++ structs.h
@@ -1,4 +1,4 @@
-#define BUCKET_SIZE 4088    // 4088 pois os outros 2 campos do bucket, juntos, ocupam 8 bytes. 4096 bytes(1 bloco) - 8 bytes = 4088 bytes.
+#define BUCKET_SIZE 4092    // 4092 pois o outro campo do bucket ocupa 4 bytes. 4096 bytes(1 bloco) - 4 bytes = 4092 bytes.
 #define NUMBER_OF_BUCKETS 5
 
 struct registro_t {
@@ -15,19 +15,10 @@ struct registro_t {
 };
 
 struct bloco_t {
-    int num_bucket;             // número do bucket
-    int tipo_bucket;            // 0 = principal, 1 = overflow
-    registro_t registros[BUCKET_SIZE];     // array de registros com 4088 bytes
-};
-
-// Nosso bucket ocupará 2 blocos do disco
-struct bucket_t {
-    int end_bloco1;           // endereco do bloco 1
-    int end_bloco2;           // endereco do bloco 2
-    bucket_t *proxOverflow;     // ponteiro para o próximo bucket de overflow, caso exista
+    bloco_t *proxOverflow;      // ponteiro para o próximo bucket de overflow, caso exista
+    registro_t registros[BUCKET_SIZE];     // array de registros com 4092 bytes
 };
 
 struct arquivo_t {
-    bucket_t buckets[NUMBER_OF_BUCKETS];
+    bloco_t bucket[NUMBER_OF_BUCKETS];
 };
-
