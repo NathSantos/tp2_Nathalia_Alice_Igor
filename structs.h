dfc4bf774a3a917ebab8e2b1d56f1186919089cd
Nathália Santos <63311872+NathSantos@users.noreply.github.com>
2023-04-26 12:12:03 -0400

    Add files via upload

--- structs.h
+++ structs.h
@@ -1,10 +1,14 @@
 struct registro_t {
     int id;
+    int tam_titulo;     // tamanho do campo título
     char titulo[301]; 
     int ano;
+    int tam_autores;    // tamanho do campo autores
     char autores[151];
     int citacoes;
+    int tam_atualizacao;    // tamanho do campo atualização
     char atualizacao[20];
+    int tam_snippet;        // tamanho do campo snippet
     char snippet[1025];
 };
 
@@ -13,4 +17,7 @@ struct bucket_t {
     int tipo_bucket;        // 0 = principal, 1 = overflow
     registro_t *registro1;  // ponteiro para o registro 1
     registro_t *registro2;  // ponteiro para o registro 2
+    registro_t *registro3;  // ponteiro para o registro 3
+    registro_t *registro4;  // ponteiro para o registro 4
+    bucket_t *proxOverflow;      // ponteiro para o próximo bucket de overflow, caso exista
 };
--- upload.cpp
+++ upload.cpp
@@ -3,136 +3,49 @@
 #include <cstring>
 #include <ctime>
 #include <cstdlib>
-
-// PARA COMPILAR E EXECUTAR O CÓDIGO:
-//      $ g++ upload.cpp -o upload
-//      $ ./upload arquivo_de_entrada.csv
+#include "structs.h"
 
 using namespace std;
 
-// TAMANHO DA TABELA HASH (ALTERE AQUI CASO DESEJAR)
-int tabela_hash_tam = 10;
+#define BUCKET_SIZE 8192
+#define NUMBER_OF_BUCKETS 5000
 
-// Definição da estrutura do registro
-struct registro_t {
-    int id;
-    char titulo[301];
-    int ano;
-    char autores[151];
-    int citacoes;
-    char atualizacao[20];
-    char snippet[1025];
-};
-
-// Definição da estrutura de uma lista encadeada de registros
-struct lista_registros_t {
-    registro_t *registro;
-    lista_registros_t *proximo;
-};
-
-typedef lista_registros_t** tabela_hash_t;
-
-// Função que adiciona um novo registro em uma lista encadeada de registros
-bool lista_registros_adicionar(registro_t *registro, lista_registros_t **lista) {
-    lista_registros_t *novo_registro = new lista_registros_t;
-    if (novo_registro == nullptr) return false;
-    novo_registro->registro = registro;
-    novo_registro->proximo = *lista;
-    *lista = novo_registro;
-    return true;
-}
-
-// Função que cria a tabela hash 
-tabela_hash_t tabela_hash_criar() {
-    tabela_hash_t tabela = new lista_registros_t*[tabela_hash_tam];
-    for(int i=0; i<tabela_hash_tam; i++) {
-        tabela[i] = nullptr;
-    }
-
-    return tabela;
-}
-
-// Função que calcula o índice do registro
-int calcular_indice(registro_t *registro) {
-    return(registro->id % tabela_hash_tam);
-}
-
-// Função que adiciona um novo registro na tabela hash
-void tabela_hash_registros_adicionar(registro_t *registro, tabela_hash_t tabela_hash) {
-    int indice_lista = calcular_indice(registro);
-    lista_registros_t *lista = tabela_hash[indice_lista];
-    lista_registros_adicionar(registro, &tabela_hash[indice_lista]);
-}
-
-// Função que lista os registros em uma dada posição da tabela
-void lista_registros_listar(lista_registros_t *lista){
-    lista_registros_t *lista_aux;
-    int i=1;
-    for(lista_aux = lista; lista_aux != nullptr; lista_aux = lista_aux->proximo, i++) {
-        cout << "- ID: " << lista_aux->registro->id << endl;
-        cout << "- Titulo: " << lista_aux->registro->titulo << endl;
-        cout << "- Ano: " << lista_aux->registro->ano << endl;
-        cout << "- Autores: " << lista_aux->registro->autores << endl;
-        cout << "- Citacoes: " << lista_aux->registro->citacoes << endl;
-        cout << "- Atualizacao: " << lista_aux->registro->atualizacao << endl;
-        cout << "- Snippet: " << lista_aux->registro->snippet << endl;
-        cout << "-----------------------------------------------" << endl;
-    } 
-}
-
-// Função que lista as posições da tabela
-void tabela_hash_registros_listar(tabela_hash_t tabela_hash) {
-    for(int i=0;i<tabela_hash_tam;i++) {
-        cout << ">>>>>>>>>> POSIÇÃO " << i << " DA TABELA HASH: <<<<<<<<<<" << endl;
-        lista_registros_listar(tabela_hash[i]);
-    }
-}
-
-// Função que libera a memória de todos os registros da tabela
-void tabela_hash_liberar_memoria(tabela_hash_t tabela_hash) {
-    for(int i = 0; i < tabela_hash_tam; i++) {
-        lista_registros_t *lista = tabela_hash[i];
-        while (lista != nullptr) {
-            lista_registros_t *proxima_lista = lista->proximo;
-            delete lista->registro;
-            delete lista;
-            lista = proxima_lista;
-        }
-    }
-    delete[] tabela_hash;
+// Função que calcula o hash de um registro com base no ID e retorna o número do bucket correspondente
+int funcaoHash(int id) {
+    return id % NUMBER_OF_BUCKETS;
 }
 
 int main(int argc, char *argv[]) {
 
-    FILE *arq;
-    arq = fopen(argv[1], "r");
-    tabela_hash_t tabela = tabela_hash_criar();
+    ifstream arq(argv[1]); // abre o arquivo .csv
 
-    if (arq == nullptr){
-        printf("Problemas na leitura do arquivo\n");
-        return 0;
+    registro_t registro;
+    string linha;
+
+    while (getline(arq, linha)) {
+        // Extrai os campos da linha lida do arquivo CSV e preenche a struct registro_t correspondente
+        sscanf(linha.c_str(), "\"%d\";\"%[^\"]\";\"%d\";\"%[^\"]\";\"%d\";\"%[^\"]\";\"%[^\"]\"", 
+        &registro.id, registro.titulo, &registro.ano, registro.autores, &registro.citacoes, 
+        registro.atualizacao, registro.snippet);
+
+        registro.tam_titulo = strlen(registro.titulo);
+        registro.tam_autores = strlen(registro.autores);
+        registro.tam_atualizacao = strlen(registro.atualizacao);
+        registro.tam_snippet = strlen(registro.snippet);
+
+        cout << registro.id << endl;
+        cout << "tamanho do titulo: " << registro.tam_titulo << endl;
+        if(registro.tam_titulo != 0) {
+            cout << "titulo: " << registro.titulo << endl;
+        }
+        cout << registro.ano << endl;
+        cout << registro.tam_autores << endl;
+        cout << registro.autores << endl;
+        cout << registro.citacoes << endl;
+        cout << registro.tam_atualizacao << endl;
+        cout << registro.atualizacao << endl;
+        cout << registro.tam_snippet << endl;
+        cout << registro.snippet << endl;
     }
 
-    while (feof(arq) == false){
-        int id, ano, citacoes;
-        char titulo[301], autores[151], atualizacao[20], snippet[1025];
-
-        fscanf(arq, "\"%d\";\"%300[^\"]\";\"%d\";\"%150[^\"]\";\"%d\";\"%19[^\"]\";\"%1024[^\"]\"\n",
-           &id, titulo, &ano, autores, &citacoes, atualizacao, snippet);
-
-        registro_t *novo_registro = new registro_t;
-        novo_registro->id = id;
-        strcpy(novo_registro->titulo,titulo);
-        novo_registro->ano = ano;
-        strcpy(novo_registro->autores,autores);
-        novo_registro->citacoes = citacoes;
-        strcpy(novo_registro->atualizacao,atualizacao);
-        strcpy(novo_registro->snippet,snippet);
-        
-        tabela_hash_registros_adicionar(novo_registro, tabela);
-    }
-    tabela_hash_registros_listar(tabela);   
-    tabela_hash_liberar_memoria(tabela);
-    fclose(arq);
-    return 1;    
 }
