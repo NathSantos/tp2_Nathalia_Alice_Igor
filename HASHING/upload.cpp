8324fa5763dda8590766b732dbfb710f728ccf5d
Nathália Santos <63311872+NathSantos@users.noreply.github.com>
2023-04-26 21:26:24 -0400

    Add files via upload

--- HASHING/upload.cpp
+++ HASHING/upload.cpp
@@ -8,18 +8,37 @@
 
 using namespace std;
 
+#define BUCKET_SIZE 4096  
+#define NUMBER_OF_BUCKETS 5
+
+struct registro_t {
+    int id;
+    int tam_titulo;     // tamanho do campo título
+    char titulo[301]; 
+    int ano;
+    int tam_autores;    // tamanho do campo autores
+    char autores[151];
+    int citacoes;
+    char atualizacao[20];
+    int tam_snippet;        // tamanho do campo snippet
+    char snippet[1025];
+};
+
+struct bloco_t {
+    int endereco;       // endereço do bloco na memória secundária
+    bloco_t *proximo;   // ponteiro para o próximo bloco de overflow, caso exista
+    registro_t registros[BUCKET_SIZE - sizeof(bloco_t*) - sizeof(int)];
+};
+
+struct bucket_t {
+    int endereco_bloco; // endereço do bloco na memória secundária
+};
+
+typedef bloco_t** tabela_hash_t;
+
 // Função que calcula o hash de um registro com base no ID e retorna o número do bucket correspondente
 int funcaoHash(int id) {
     return id % NUMBER_OF_BUCKETS;
-}
-
-// Função que cria a tabela hash 
-tabela_hash_t tabela_hash_criar() {
-    tabela_hash_t tabela = new bloco_t*[NUMBER_OF_BUCKETS];
-    for(int i=0; i<NUMBER_OF_BUCKETS; i++) {
-        tabela[i] = nullptr;
-    }
-    return tabela;
 }
 
 void insere_registro_overflow(registro_t registro, bloco_t *bloco) {
@@ -79,109 +98,81 @@ void insere_registro(registro_t registro, tabela_hash_t tabela) {
     }
 }
 
+// PESQUISAR COMO ALTERAR AS FUNÇÕES DE INSERIR PARA FAZER A TABELA HASH COM BUCKET AO INVES DE BLOCO!!
 
 int main(int argc, char *argv[]) {
 
-    ifstream arq(argv[1]); // abre o arquivo .csv
+    ifstream arq(argv[1]); // abre o arquivo de entrada passado como argumento
+    fstream file("arquivo_dados.bin", ios::out | ios::binary);  // abre o arquivo binário para escrita em disco
 
-    // Abrir o arquivo binário para escrita
-    fstream file("arquivo_dados.bin", ios::out | ios::binary);
-    
-    arquivo_t arquivo;
-    inicializaBuckets(&arquivo, file);
+    string linha;
 
+    while (getline(arq, linha)) {
+        registro_t *registro = new registro_t();
+        stringstream ss(linha);
+        string campo;
 
-    // Fechar o arquivo de escrita
-    file.close();
-    
-    // Abrir o arquivo binário para leitura
-    ifstream file_read("arquivo_dados.bin", ios::in | ios::binary);
-    
-    // Ler os dados do arquivo e armazená-los na estrutura de dados em memória
-    arquivo_t arquivo_read;
-    file_read.read(reinterpret_cast<char*>(&arquivo_read), sizeof(arquivo_t));
-    
-    // Printar o endereço de memória inicial de cada bucket
-    for (int i = 0; i < NUMBER_OF_BUCKETS; i++) {
-        cout << "Endereço de memória do bucket " << i << ": " << &arquivo_read.buckets[i] << endl;
-        cout << "Tamanho do bucket " << i << ": " << sizeof(arquivo_read.buckets[i]) << endl;
-        cout << "Tamanho do bucket " << i << ": " << sizeof(arquivo_read.buckets[i].registro1) << endl;
-        cout << "Tamanho do bucket " << i << ": " << sizeof(arquivo_read.buckets[i].registro2) << endl;
+        getline(ss, campo, ';');
+        if (!campo.empty()) {
+            campo = campo.substr(1, campo.length() - 2); // remove as aspas da string
+            registro->id = stoi(campo);
+        }
+
+        getline(ss, campo, ';');
+        if (!campo.empty()) {
+            campo = campo.substr(1, campo.length() - 2); // remove as aspas da string
+            strncpy(registro->titulo, campo.c_str(), sizeof(registro->titulo));
+            registro->titulo[sizeof(registro->titulo) - 1] = '\0';
+            registro->tam_titulo = strlen(registro->titulo);
+        }
+
+        getline(ss, campo, ';');
+        if (!campo.empty()) {
+            campo = campo.substr(1, campo.length() - 2); // remove as aspas da string
+            registro->ano = stoi(campo);
+        }
+
+        getline(ss, campo, ';');
+        if (!campo.empty()) {
+            campo = campo.substr(1, campo.length() - 2); // remove as aspas da string
+            strncpy(registro->autores, campo.c_str(), sizeof(registro->autores));
+            registro->autores[sizeof(registro->autores) - 1] = '\0';
+            registro->tam_autores = strlen(registro->autores);
+        }
+
+        getline(ss, campo, ';');
+        if (!campo.empty()) {
+            campo = campo.substr(1, campo.length() - 2); // remove as aspas da string
+            registro->citacoes = stoi(campo);
+        }
+
+        getline(ss, campo, ';');
+        if (!campo.empty()) {
+            campo = campo.substr(1, campo.length() - 2); // remove as aspas da string
+            strncpy(registro->atualizacao, campo.c_str(), sizeof(registro->atualizacao));
+            registro->atualizacao[sizeof(registro->atualizacao) - 1] = '\0';
+        }
+
+        getline(ss, campo);
+        if (campo != "NULL") {
+            campo = campo.substr(1, campo.length() - 2); // remove as aspas da string
+            strncpy(registro->snippet, campo.c_str(), sizeof(registro->snippet));
+            registro->snippet[sizeof(registro->snippet) - 1] = '\0';
+            registro->tam_snippet = strlen(registro->snippet);
+        }
+
+        cout << "ID: " << registro->id << endl;
+        cout << "Tam Titulo: " << registro->tam_titulo << endl;
+        cout << "Titulo: " << registro->titulo << endl;
+        cout << "Ano: " << registro->ano << endl;
+        cout << "Tam Autores: " << registro->tam_autores << endl;
+        cout << "Autores: " << registro->autores << endl;
+        cout << "Citacoes: " << registro->citacoes << endl;
+        cout << "Att: " << registro->atualizacao << endl;
+        cout << "Tam Snippet: " << registro->tam_snippet << endl;
+        cout << "Snippet: " << registro->snippet << endl;
+        
+        delete registro;
     }
-    
-    // Fechar o arquivo de leitura
-    file_read.close();
-    
-    return 0;
-    
-    // string linha;
-
-    // while (getline(arq, linha)) {
-    //     registro_t registro;
-    //     stringstream ss(linha);
-    //     string campo;
-
-    //     getline(ss, campo, ';');
-    //     if (!campo.empty()) {
-    //         campo = campo.substr(1, campo.length() - 2); // remove as aspas da string
-    //         registro.id = stoi(campo);
-    //     }
-
-    //     getline(ss, campo, ';');
-    //     if (!campo.empty()) {
-    //         campo = campo.substr(1, campo.length() - 2); // remove as aspas da string
-    //         strncpy(registro.titulo, campo.c_str(), sizeof(registro.titulo));
-    //         registro.titulo[sizeof(registro.titulo) - 1] = '\0';
-    //         registro.tam_titulo = strlen(registro.titulo);
-    //     }
-
-    //     getline(ss, campo, ';');
-    //     if (!campo.empty()) {
-    //         campo = campo.substr(1, campo.length() - 2); // remove as aspas da string
-    //         registro.ano = stoi(campo);
-    //     }
-
-    //     getline(ss, campo, ';');
-    //     if (!campo.empty()) {
-    //         campo = campo.substr(1, campo.length() - 2); // remove as aspas da string
-    //         strncpy(registro.autores, campo.c_str(), sizeof(registro.autores));
-    //         registro.autores[sizeof(registro.autores) - 1] = '\0';
-    //         registro.tam_autores = strlen(registro.autores);
-    //     }
-
-    //     getline(ss, campo, ';');
-    //     if (!campo.empty()) {
-    //         campo = campo.substr(1, campo.length() - 2); // remove as aspas da string
-    //         registro.citacoes = stoi(campo);
-    //     }
-
-    //     getline(ss, campo, ';');
-    //     if (!campo.empty()) {
-    //         campo = campo.substr(1, campo.length() - 2); // remove as aspas da string
-    //         strncpy(registro.atualizacao, campo.c_str(), sizeof(registro.atualizacao));
-    //         registro.atualizacao[sizeof(registro.atualizacao) - 1] = '\0';
-    //     }
-
-    //     getline(ss, campo);
-    //     cout << "------ TESTE: " << campo << endl;
-    //     if (campo != "NULL") {
-    //         cout << "OLAAAAAAAAAAAAAAAAAAAAAAAA" << endl;
-    //         campo = campo.substr(1, campo.length() - 2); // remove as aspas da string
-    //         strncpy(registro.snippet, campo.c_str(), sizeof(registro.snippet));
-    //         registro.snippet[sizeof(registro.snippet) - 1] = '\0';
-    //         registro.tam_snippet = strlen(registro.snippet);
-    //     }
-
-    //     cout << "ID: " << registro.id << endl;
-    //     cout << "Tam Titulo: " << registro.tam_titulo << endl;
-    //     cout << "Titulo: " << registro.titulo << endl;
-    //     cout << "Ano: " << registro.ano << endl;
-    //     cout << "Tam Autores: " << registro.tam_autores << endl;
-    //     cout << "Autores: " << registro.autores << endl;
-    //     cout << "Citacoes: " << registro.citacoes << endl;
-    //     cout << "Att: " << registro.atualizacao << endl;
-    //     cout << "Tam Snippet: " << registro.tam_snippet << endl;
-    //     cout << "Snippet: " << registro.snippet << endl;
-    // }
 
 }
