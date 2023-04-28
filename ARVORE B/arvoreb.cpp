84bffbf04400601cb19a5da57d145ac0c0d4940d
Alice Lucena <85039426+sliceofalice@users.noreply.github.com>
2023-04-28 15:08:36 -0400

    Add files via upload

---
+++ ARVORE B/arvoreb.cpp
@@ -0,0 +1,15 @@
+//////////////////////////
+//      ÁRVORE B+       //
+//////////////////////////
+
+// INCLUDES
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+// MAIN FUNCTION
+int main(){
+    cout << "Hello World" << endl;
+} // end main
