27b5487d1d08e8730992b04d540a283e1355d177
Alice Lucena <85039426+sliceofalice@users.noreply.github.com>
2023-04-29 21:44:44 -0400

    Update arvoreb.cpp

--- ARVORE B/arvoreb.cpp
+++ ARVORE B/arvoreb.cpp
@@ -1,5 +1,5 @@
 //////////////////////////////////////////
-//              ÁRVORE B+               //
+//               B+ TREE                //
 //////////////////////////////////////////
 
 //======================================//
@@ -8,40 +8,44 @@
 
 using namespace std;
 
-#define M 4
+#define ORDER 4
 
-//======================================//
-// TIPOS
-
-// TIPO NÓ
+// TYPE NODE
 typedef struct node_t{
-        int n; // número de chaves no nó
-        int keys[M-1]; // chaves do nó
-        struct node *children[M]; // ponteiro para filhos do nó
-        int leaf; // indica se nó é folha (1) ou não (0)
+        int n; // number of keys
+        int keys[ORDER - 1];
+        struct node_t * children[ORDER];
+        int leaf; // set to 1 if node is leaf
 }node_t;
 
-// TIPO ÁRVORE
+// TIPO TREE
 typedef struct bplus_t{
         node_t *root;
 }bplus_t;
 
-//======================================//
-// FUNÇÕES
+int tmp;
 
-// CRIA NÓ DA ÁRVORE B+
+// CREATE EMPTY NODE
 node_t * create_node() {
 
-    node_t * new_node = (node_t*) new node_t; // aloca novo nó
+    node_t * new_node = new node_t; // space for node
 
-    new_node->n = 0; // define número de chaves inicial
-    new_node->leaf = 1; // define que é folha
-    for (int i = 0; i < M; i++) {
-        new_node->child[i] = NULL; // define filhos
-    } // end for
+    new_node->n = 0; // number of keys
+    new_node->leaf = 1; // node is leaf
 
-    return new_node; // retorna novo nó
+    return new_node; // return new node
+
 } // end
+
+// PUT KEY IN NODE
+node_t * put_key_in_node(node_t * node, int key){
+
+        if (node->n == 0){
+                node->keys[0] = key;
+                node->n++;
+        }
+        return node;
+}
 
 //======================================//
 
@@ -52,11 +56,25 @@ node_t * create_node() {
 // MAIN FUNCTION
 int main(){
 
-        key_t new_key;
+        int new_key;
+
+        node_t * new_node = create_node();
 
         while (new_key != -1){
                 cout << "Nova Chave: ";
                 cin >> new_key;
+
+                // first number is 1
+                new_node = put_key_in_node(new_node, new_key);
+
+                cout << new_node->n << endl;
+                for (int i = 0; i < ORDER - 1; i++){
+                        cout << new_node->keys[i] << endl; 
+                } // end for i
+                for (int j = 0; j < ORDER; j++){
+                        cout << new_node->children[j] << endl; 
+                } // end for j
+                cout << new_node->leaf << endl << endl;
         } // end while
 
         return 0;
