dc3a9932a2ff475e7a55d01d4921dd56c0fa3dc1
Alice Lucena <85039426+sliceofalice@users.noreply.github.com>
2023-05-01 17:36:34 -0400

    Add files via upload

---
+++ PRIMARY INDEX/bplus.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include "bplustree.h"
+#include "structs.h"
+
+using namespace std;
+
+BPTree bpt;
+
+void ler_arquivo_binario() {
+    ifstream arquivo("arquivo_dados.bin", ios::in | ios::binary);
+
+    if (!arquivo) {
+        cerr << "Erro ao abrir o arquivo!" << endl;
+        return;
+    }
+
+    int bloco_size = sizeof(bloco_t);
+
+    arquivo.seekg(0, ios::end);
+    int tamanho_arquivo = arquivo.tellg();
+    arquivo.seekg(0);
+
+    cout << "Tamanho do arquivo: " << tamanho_arquivo << endl;
+    cout << "\n" << endl;
+
+    int num_blocos = tamanho_arquivo / bloco_size;
+    for (int i = 0; i < num_blocos; i++) {
+        bloco_t bloco_lido;
+        arquivo.read((char*)&bloco_lido, bloco_size);
+        arquivo.clear();
+        cout << "\n============ BLOCO " << i << " ============" << endl;
+        for (int j = 0; j < NUMBER_OF_REGISTROS; j++) {
+			bpt.insert(bloco_lido.registros[j].id);
+        }
+    }
+    arquivo.close();
+}
+
+int main(int argc, char* argv[])
+{
+	
+	ler_arquivo_binario();
+
+	cout << "\n\n" << endl;
+
+	bpt.display(bpt.getRoot());
+
+	bpt.search(7);
+	//bpt.search(8);
+	//bpt.search(2);
+
+	return 0;
+}
---
+++ PRIMARY INDEX/structs.h
@@ -0,0 +1,27 @@
+#define BLOCO_SIZE 4096         // tamanho do bloco em bytes
+#define NUMBER_OF_BUCKETS 5     // número de buckets na tabela hash
+#define NUMBER_OF_REGISTROS 2   // número de registros por bloco
+
+struct registro_t {
+    int id;
+    char titulo[301]; 
+    int ano;
+    char autores[151];
+    int citacoes;
+    char atualizacao[20];
+    char snippet[1025];
+};
+
+struct bloco_t {
+    registro_t registros[NUMBER_OF_REGISTROS];
+    unsigned char ocupa[BLOCO_SIZE - (sizeof(registro_t) * NUMBER_OF_REGISTROS)];
+};
+
+struct bucket_t {
+    int endereco_bloco1;            // endereço do bloco 1 na memória secundária
+    int num_registros_atualmente1;  // número de registros atualmente no bloco 1
+    int endereco_bloco2;            // endereço do bloco 2 na memória secundária
+    int num_registros_atualmente2;  // número de registros atualmente no bloco 2
+};
+
+typedef bucket_t** tabela_hash_t;
