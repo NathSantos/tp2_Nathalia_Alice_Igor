af1c8b89a64bfc26621a2b3fa55e46d2f07eae1f
Alice Lucena <85039426+sliceofalice@users.noreply.github.com>
2023-05-03 10:01:34 -0400

    Update bplus.cpp

--- PRIMARY INDEX/bplus.cpp
+++ PRIMARY INDEX/bplus.cpp
@@ -7,7 +7,7 @@ using namespace std;
 BPTree bpt;
 
 void ler_arquivo_binario() {
-    ifstream arquivo("arquivo_dados.bin", ios::in | ios::binary);
+    ifstream arquivo("arquivo_dados20.bin", ios::in | ios::binary);
 
     if (!arquivo) {
         cerr << "Erro ao abrir o arquivo!" << endl;
@@ -20,15 +20,14 @@ void ler_arquivo_binario() {
     int tamanho_arquivo = arquivo.tellg();
     arquivo.seekg(0);
 
-    cout << "Tamanho do arquivo: " << tamanho_arquivo << endl;
-    cout << "\n" << endl;
+    // cout << "Tamanho do arquivo: " << tamanho_arquivo << endl << "\n";
 
     int num_blocos = tamanho_arquivo / bloco_size;
     for (int i = 0; i < num_blocos; i++) {
         bloco_t bloco_lido;
         arquivo.read((char*)&bloco_lido, bloco_size);
         arquivo.clear();
-        cout << "\n============ BLOCO " << i << " ============" << endl;
+        // cout << "============ BLOCO " << i << " ============" << endl;
         for (int j = 0; j < NUMBER_OF_REGISTROS; j++) {
 			bpt.insert(bloco_lido.registros[j].id);
         }
@@ -41,13 +40,22 @@ int main(int argc, char* argv[])
 	
 	ler_arquivo_binario();
 
-	cout << "\n\n" << endl;
+    bpt.getLeaf(bpt.getRoot());
+    
+    // cout << MAX << endl;
 
-	bpt.getLeaf(bpt.getRoot());
+    for (int i = 0; i < 4; i++){
+        cout << &leaves[i] << ": ";
+        for (int j = 0; j < leaves[i].size; j++){
+            cout << (leaves[i]).key[j] << " ";
+        }
+        (leaves[i]).next = &leaves[i+1];
+        cout << "-- "<< leaves[i].next;
+        cout << "\n";
+    }
 
 	bpt.search(4);
 	//bpt.search(8);
 	//bpt.search(2);
-
 	return 0;
 }
